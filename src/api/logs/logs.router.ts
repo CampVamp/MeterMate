7cc8cbb2389c56e6a8000e5de34de7b0449b0272
Ajay <ajayramsaravanan4@gmail.com>
2024-10-26 10:10:47 +0530

    fix: Build issues one more time

--- src/api/logs/logs.router.ts
+++ src/api/logs/logs.router.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getAllUserLogs, createUserLog } from "./logs.controller";
+import { getAllUserLogs, createLog } from "./logs.controller";
 import { authenticateJWT } from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/", authenticateJWT, getAllUserLogs);
-router.post("/", authenticateJWT, createUserLog);
+router.post("/", authenticateJWT, createLog);
